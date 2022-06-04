e7fb12ebca11a8de3a1820d30e5dff95da36b10f
Alissa <alissamak@outlook.com>
2022-06-04 16:12:24 -0400

    added alt solution to npx start error in bin

--- bin.js
+++ bin.js
@@ -2,13 +2,13 @@
 const open = require('open')
 const { exec, spawn } = require('child_process')
 exec('npx webpack --mode="development"', () => {
-    // spawn('node', [ 'webpack', '--watch', '--mode="development"' ], { stdio: 'inherit' });
-    spawn('node', ['index.js'], {
-        env: {
-            NODE_ENV: 'production',
-            PATH: process.env.PATH
-        }
-    })
+    spawn('npx.cmd', [ 'webpack', '--watch', '--mode="development"' ], { stdio: 'inherit' });
+    // spawn('node', ['index.js'], {
+    //     env: {
+    //         NODE_ENV: 'production',
+    //         PATH: process.env.PATH
+    //     }
+    // })
     open('./public/index.html')
     console.log('Watching for changes in "./src"')
 })
